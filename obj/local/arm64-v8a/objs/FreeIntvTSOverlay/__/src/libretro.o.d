b2021070df1e39a4b8bed78798b48ea47edbca22
Jason Carr <jason.c.carr@gmail.com>
2025-10-29 15:12:12 -0400

    Checked compile on Android, Windows, and Linux

--- obj/local/arm64-v8a/objs/FreeIntvTSOverlay/__/src/libretro.o.d
+++ obj/local/arm64-v8a/objs/FreeIntvTSOverlay/__/src/libretro.o.d
@@ -1,36 +1,30 @@
-S:/VisualStudio/FreeIntvTSOverlay/obj/local/arm64-v8a/objs/FreeIntvTSOverlay/__/src/libretro.o: \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/libretro.c \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/libretro.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/libretro_core_options.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/retro_inline.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/libretro_core_options_intl.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/file/file_path.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/retro_common_api.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/boolean.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/retro_miscellaneous.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/intv.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/cp1610.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/memory.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/stic.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/psg.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/ivoice.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/controller.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/osd.h \
-  S:/VisualStudio/FreeIntvTSOverlay/jni/../src/stb_image.h
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/libretro.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/libretro_core_options.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/retro_inline.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/libretro_core_options_intl.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/file/file_path.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/retro_common_api.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/boolean.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/deps/libretro-common/include/retro_miscellaneous.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/intv.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/cp1610.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/memory.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/stic.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/psg.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/ivoice.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/controller.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/osd.h:
-S:/VisualStudio/FreeIntvTSOverlay/jni/../src/stb_image.h:
+./obj/local/arm64-v8a/objs/FreeIntvTSOverlay/__/src/libretro.o: \
+  jni/../src/libretro.c \
+  jni/../src/deps/libretro-common/include/libretro.h \
+  jni/../src/libretro_core_options.h \
+  jni/../src/deps/libretro-common/include/retro_inline.h \
+  jni/../src/libretro_core_options_intl.h \
+  jni/../src/deps/libretro-common/include/file/file_path.h \
+  jni/../src/deps/libretro-common/include/retro_common_api.h \
+  jni/../src/deps/libretro-common/include/boolean.h \
+  jni/../src/deps/libretro-common/include/retro_miscellaneous.h \
+  jni/../src/intv.h jni/../src/cp1610.h jni/../src/memory.h \
+  jni/../src/stic.h jni/../src/psg.h jni/../src/ivoice.h \
+  jni/../src/controller.h jni/../src/osd.h jni/../src/stb_image.h
+jni/../src/deps/libretro-common/include/libretro.h:
+jni/../src/libretro_core_options.h:
+jni/../src/deps/libretro-common/include/retro_inline.h:
+jni/../src/libretro_core_options_intl.h:
+jni/../src/deps/libretro-common/include/file/file_path.h:
+jni/../src/deps/libretro-common/include/retro_common_api.h:
+jni/../src/deps/libretro-common/include/boolean.h:
+jni/../src/deps/libretro-common/include/retro_miscellaneous.h:
+jni/../src/intv.h:
+jni/../src/cp1610.h:
+jni/../src/memory.h:
+jni/../src/stic.h:
+jni/../src/psg.h:
+jni/../src/ivoice.h:
+jni/../src/controller.h:
+jni/../src/osd.h:
+jni/../src/stb_image.h:
